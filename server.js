e1ed31cdd60b26fadf4add3b78cec235b5486869
Shashank kumar Patwa <59040037+nshnkzz@users.noreply.github.com>
2021-03-11 11:26:08 +0530

    changes

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+// app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send("Success") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
